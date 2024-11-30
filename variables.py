974dfa39346a5e22b086e56f57f314239f00adae
unknown <jjyankson19@gmail.com>
2024-11-29 16:35:15 -1200

    conditional statements in python

--- variables.py
+++ variables.py
@@ -8,3 +8,14 @@ device_id = "ju2ey12"
 data_type = type(device_id)
 print(data_type)
 #result should be string.
+
+#demonstrate a type error 
+# radio_frequency = "23.3/hz"
+# number = 4
+# print(radio_frequency + number)
+
+#reassigning a variable
+telecom_id = "r18js72"
+print(telecom_id)
+telecom_id = "ns5966"
+print(telecom_id)
