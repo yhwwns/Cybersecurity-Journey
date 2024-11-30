934d19f72085b4ccde3298693dfc330fcb53fc1a
unknown <jjyankson19@gmail.com>
2024-11-29 16:17:41 -1200

    variables.py

--- index.py
+++ index.py
@@ -18,8 +18,28 @@ print(3 + 5)
 # check for account balance
 
 print(10.3 + 4)
+print(20/2)
 
 #boolean 
 #compare two nums
 
 print(5 > 4)
+
+#tuples 
+#configured softwares 
+print(("splunk", "google chronicle" "splunk enterprise"))
+
+#dictionary data that consist of one or more key value pairs
+print(
+    {
+        1 : "East",
+        2 : "West",
+        3 : "South",
+        4 : "North"
+    }
+)
+
+#sets
+print({
+    "apple", "pear", "banana"
+})
---
+++ variables.py
@@ -0,0 +1,10 @@
+#assign a variable 
+device_Id = "h32rb17"
+print("The device", device_Id, "has been breached!")
+print("m50pi31")
+
+#type function
+device_id = "ju2ey12"
+data_type = type(device_id)
+print(data_type)
+#result should be string.
