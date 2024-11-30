0d7ab9268209af5dcdf7cf619cd1c3f158607228
unknown <jjyankson19@gmail.com>
2024-11-29 15:15:04 -1200

    data types in python

---
+++ index.py
@@ -0,0 +1,25 @@
+#first line of python code
+print("hello. world!")
+
+#data types (strings, list. integers, floats and booleans etc)
+#strings
+print("what's your name bro?")
+
+#lists 
+#list of people to assess the data
+print(["Christian", "Helenda", "Jefferson"])
+
+#integers 
+#add the sum of two nums
+
+print(3 + 5)
+
+# floats
+# check for account balance
+
+print(10.3 + 4)
+
+#boolean 
+#compare two nums
+
+print(5 > 4)
